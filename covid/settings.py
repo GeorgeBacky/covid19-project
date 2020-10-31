92be8b00e82da9a533ac66a347c71e4f34e29dc6
Panagiotis <1qk231@gmail.com>
2020-10-31 21:50:12 +0200

    NOW ITS COOL AF

--- covid/settings.py
+++ covid/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,13 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'covidapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'covidapp',
 ]
 
 MIDDLEWARE = [
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'covid.urls'
 
+TEMPLATES_DIR = BASE_DIR.joinpath('templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'el-gr'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath('static')
+]
--- covidapp/views.py
+++ covidapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 import json
+import asyncio
 
 url = "https://rapidapi.p.rapidapi.com/statistics"
 
@@ -9,31 +10,39 @@ headers = {
     'x-rapidapi-key': "3fb70290famsh4ee6af91c3e39cfp1f3446jsnecaaece2f9d1"
     }
 
-response = requests.request("GET", url, headers=headers).json()
+
+covid_response = requests.request("GET", url, headers=headers).json()
+
+async def data_reloader():
+    await asyncio.sleep(300)
+    covid_response = requests.request("GET", url, headers=headers).json()
+
+data_reloader()
 
 # Create your views here.
-
 def indexview(request):
-    noofresults = int(response['results'])
-    mylist = []
-    for x in range(0,noofresults):
-        mylist.append(response['response'][x]['country'])
-    if request.method == "POST":   
+    covid_data = covid_response['response']
+    noofresults = int(covid_response['results'])
+    countries = []
+    for result in covid_data:
+        countries.append(result['country'])
+    countries.sort()
+    context = {
+        'mylist': countries
+    }
+
+    if request.POST and request.POST['selectedcountry']:
         selectedcountry = request.POST['selectedcountry']
-        noofresults = int(response['results'])
-        for x in range(0,noofresults):
-            if selectedcountry == response['response'][x]['country']:
-                new = response['response'][x]['cases']['new']
-                active = response['response'][x]['cases']['active']
-                crtical = response['response'][x]['cases']['critical']
-                recovered = response['response'][x]['cases']['recovered']
-                total =  response['response'][x]['cases']['total']
-                deaths = int(total) - int(active) - int(recovered)
-        context = {'selectedcountry': selectedcountry ,'mylist': mylist,'new' : new , 'active' : active , 'critical' : crtical , 'recovered' : recovered , 'deaths' : deaths, 'total' :total}
-        return render(request, 'index.html', context)
-    noofresults = int(response['results'])
-    mylist = []
-    for x in range(0,noofresults):
-        mylist.append(response['response'][x]['country'])
-    context = {'mylist' : mylist}
+    else:
+        selectedcountry = "All"
+
+    for result in covid_data:
+        if selectedcountry == result['country']:
+            new = result['cases']['new']
+            active = result['cases']['active']
+            critical = result['cases']['critical']
+            recovered = result['cases']['recovered']
+            total =  result['cases']['total']
+            deaths = result['deaths']['total']
+    context.update({'selectedcountry': selectedcountry, 'new' : new , 'active' : active , 'critical' : critical , 'recovered' : recovered , 'deaths' : deaths, 'total' :total})
     return render(request, 'index.html', context)
